48cb97fde745061459738b4dbb2d350d6e44d575
azheng <azheng@azheng-laptop.(none)>
2013-08-13 23:52:45 +0800

    -

--- config/class_loader.config.php
+++ config/class_loader.config.php
@@ -1,17 +1,17 @@
 <?php
 //return array('app', 'lib', HYPERFRAMEWORK_PATH.'web/lib');
 
-//array(
-//  'Hft\Application' => array('app', '@folder_mapping' => false),
-//  'Hft' => array('lib'),
-//  'Hyperframework\Web' => HYPERFRAMEWORK_PATH . 'web/lib'
-//);
+return array(
+  'Hft\Application' => array('app', '@folder_mapping' => false),
+  'Hft' => array('lib'),
+  'Hyperframework\Web' => HYPERFRAMEWORK_PATH . 'web/lib'
+);
 
 return array(
   'Hft' => array('lib', 'Application' => array('app', '@folder_mapping' => false, 'Sub' => array('app/sub', '@folder_mapping' => false))),
   'Hyperframework\Web' => HYPERFRAMEWORK_PATH . 'web/lib',
   'Hft\Application' => array('lib/Application'),
   'Xxx' => array('@recursive' => false, 'app/xxx'),
-  '1XX' => array('@root' => '/etc/libp', '.', array('@exclude' => true, 'a', 'b', 'c')),
-  '2XX' => array('/etc/libp', array('@root' => '/etc/libp', 'a', 'b', 'c', '@exclude' => true)),
+  '1XX' => array('@root' => '/etc/lib', '.', array('@exclude' => true, 'a', 'b', 'c')),
+  '2XX' => array('/etc/lib', array('@root' => '/etc/lib', 'a', 'b', 'c', '@exclude' => true)),
 );
