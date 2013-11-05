5d052ebd1df07680fbd77489d5f9bdc3cd9d6263
azheng <none@google.com>
2013-11-05 18:22:47 +0800

    -

--- config/env/env.config.php
+++ config/env/env.config.php
@@ -6,7 +6,7 @@ const HYPERFRAMEWORK_PATH =
     '/home/az/daoxila_www/vendor/hf/lib/Hyperframework/';
 require HYPERFRAMEWORK_PATH . 'Config.php';
 //\Hyperframework\Config::set(
-//    ['Hyperframework\Env', ENV],
+//    ['Hyperframework\AppEnv', ENV],
 //    ['Hyperframework\Web\PathInfo\CacheEnabled', false],
 //    ['Hyperframework\Web\View\Asset\CacheEnabled', false],
 //    ['Hyperframework\ClassLoader\CacheEnabled', false]
