21257ca57bee0e9168e9c5a78352de33ea82727d
azheng <none@google.com>
2013-11-26 17:59:08 +0800

    -

--- lib/Yxj/App/Category/Brand/Action.php
+++ lib/Yxj/App/Category/Brand/Action.php
@@ -1,10 +1,10 @@
 <?php 
 class CategoryBrandAction {
-    public function PUT() {
+    public function put() {
         $categoryId = $GLOBALS['PATH_SECTION_LIST'][1][1];
         $brandId = $_GET['brand_id'];
         $pr = $_POST['pr'];
-        Db::update(
+        DbClient::update(
             'brand_category',
             array('popularity_rank' => $pr),
             'category_id = ? AND brand_id = ?', $categoryId, $brandId
