cabcd0db2e5dec803cdc9f97470e93436e3baf94
azheng <none@google.com>
2013-11-29 11:31:46 +0800

    -

--- lib/Yxj/View/Html.php
+++ lib/Yxj/View/Html.php
@@ -3,6 +3,10 @@ namespace Yxj\View;
 
 class Html {
     public function render() {
-        echo '<script src="/js/common.v323.js"></script>';
+        Asset::renderJsLink('/js/yxj/common.js');
+        // /asset/js/common.js.php
+        Asset::renderJsLink('/js/tv/common.js');
+        // /vendor/tv/public/js/tv/tv.js
+        echo '<script src="/js/common-v122.js"></script>';
     }
 }
