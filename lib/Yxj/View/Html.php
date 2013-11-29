4e9f726df480c5fef357ee71825277c1c33db8c3
azheng <none@google.com>
2013-11-29 14:49:03 +0800

    -

--- lib/Yxj/View/Html.php
+++ lib/Yxj/View/Html.php
@@ -3,10 +3,18 @@ namespace Yxj\View;
 
 class Html {
     public function render() {
-        Asset::renderJsLink('/js/yxj/common.js');
+        //default js namespace js/yxj
+        Asset::setJsNamespace('js/yxj')
+        Asset::renderJsLink('xyj/common.js');
+        // /asset/js/yxj/common.js
+        echo '<script src="/js/common.js"></script>';
+
         // /asset/js/common.js.php
-        Asset::renderJsLink('/js/tv/common.js');
-        // /vendor/tv/public/js/tv/tv.js
-        echo '<script src="/js/common-v122.js"></script>';
+        Asset::setJsNamespace('js/tv');
+        Asset::renderJsLink('xyj/common.js');
+        //vendor self render + host read vendor's manifest
+        // /vendor/tv/public/js/tv/common-v213.js
+        echo '<script src="/js/tv/common-v122.js"></script>';
+        // /js/yxj/common-v122+/js/yxj/jquery-v1.2.1.min+list-v3233.js => named => /js/yxj/combined_abc-v123.js
     }
 }
