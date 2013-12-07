3d0337d581fced3d3151696aed0c849767b7cc14
azheng <none@gmail.com>
2013-12-07 12:17:01 +0800

    -

--- asset/index.php
+++ asset/index.php
@@ -2,6 +2,6 @@
 namespace Yxj;
 
 define(__NAMESPACE__ . '\ROOT_PATH', dirname(__DIR__));
-require ROOT_PATH . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR .
-    __NAMESPACE__ . DIRECTORY_SEPARATOR . 'Runner.php';
+require ROOT_PATH . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR
+    . __NAMESPACE__ . DIRECTORY_SEPARATOR . 'Runner.php';
 Runner::runAssetProxy();
