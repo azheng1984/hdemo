89c52317e80deaa71e2620c51a5c69a494204175
azheng <none@google.com>
2014-01-15 13:51:13 +0800

    -

--- data/cache/path_info.cache.php
+++ data/cache/path_info.cache.php
@@ -2,6 +2,13 @@
 return array(
     'namespace' => 'Yxj\App',
     'paths' =>array(
-        '/' => array('views' => 'Html')
+        '/' => array('views' => 'Html'),
+        '#Error' => array(
+            'namespace' => 'Error',
+            'paths' => array(
+                'Client' => array('views' => 'Html'),
+                'Server' => array('views' => 'Html')
+            )
+        )
     )
 );
--- lib/Yxj/App/Error/Server/Html.php
+++ lib/Yxj/App/Error/Server/Html.php
@@ -2,11 +2,12 @@
 namespace Yxj\App\Error\Server;
 
 class Html {
-  public function render() {
-    if ($GLOBALS['EXCEPTION_HANDLER']->getException() === null) {
-      throw new NotFoundException;
+    public function render() {
+        var_dump(\Hyperframework\Web\ExceptionHandler::getException());
+        //if (ErrorHandler::getException() === null) {
+            //throw new NotFoundException;
+        //}
+        //echo '<h1>404 Not Found page</h1>';
+        //trigger_error($GLOBALS['UNHANDLED_EXCEPTION'], E_USER_ERROR);
     }
-    echo '<h1>404 Not Found page</h1>';
-    //trigger_error($GLOBALS['UNHANDLED_EXCEPTION'], E_USER_ERROR);
-  }
 }
